4b738150c5000841bc21c627015e17c168bc295a
Your mato <you@example.com>
2020-12-27 17:33:09 +0800

    Initial Commit

---
+++ SwiftUIButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIButton
+//
+//  Created by kittawat phuangsombat on 2020/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by kittawat phuangsombat on 2020/12/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
